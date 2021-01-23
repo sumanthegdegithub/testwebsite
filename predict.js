a85e5a766576d86330bb64919af70f341bcc1eb4
Sumant Hegde <60440915+sumanthegdegithub@users.noreply.github.com>
2021-01-23 17:21:53 +0530

    d

--- predict.js
+++ predict.js
@@ -3,7 +3,6 @@ $('#image_selector').change(function(){
 	reader.onload = function(){
 		let dataURL = reader.result;
 		$('#selected-image').attr('src',dataURL);
-		$('#prediction-list').empty();
 	}
 	let file = $('#image-selector').prop('files')[0];
 	reader.readAsDataURL(file);
@@ -11,12 +10,12 @@ $('#image_selector').change(function(){
 
 let model;
 (async function(){
-	model = await tf.loadModel('model/model.json');
+	model = await tf.loadLayersModel('model/model.json');
 })();
 
 
 $('#predict-button').click(async function(){
-  let image = $('#myImage').get(0);
+  let image = $('#selected-image').get(0);
   let tensor = tf.fromPixels(image).reshape([1, 96, 96, 1]).toFloat();
 
 
